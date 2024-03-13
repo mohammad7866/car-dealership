40c324e74e4db726fb7a13f9b1e33cf9448cc460
Mohammad A khasiya <mohammad.a.khasiya@uk.pwc.com>
2024-03-13 01:30:50 +0000

    final

--- backend/BMWapi/Tests/AuthControllerTests.cs
+++ backend/BMWapi/Tests/AuthControllerTests.cs
@@ -1,6 +1,33 @@
-﻿namespace BMWApi.Tests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using BMWApi.Controllers;
+using BMWApi.Models;
+using BMWApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BMWApi.Tests.Controllers
 {
+    [TestClass]
     public class AuthControllerTests
     {
+        [TestMethod]
+        public void Login_InvalidCredentials_ReturnsUnauthorized()
+        {
+            // Arrange
+            var mockAuthService = new Mock<AuthService>(); // Mocking the concrete AuthService
+            mockAuthService.Setup(service => service.AuthenticateUser(It.IsAny<string>(), It.IsAny<string>()))
+                           .Returns(false); // Mock the AuthenticateUser method to always return false
+
+            var controller = new AuthController(mockAuthService.Object); // Pass the mocked AuthService to the controller
+
+            // Act
+            var loginResult = controller.Login(new LoginModel { Username = "invalidUser", Password = "invalidPassword" }) as UnauthorizedObjectResult;
+
+            // Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(loginResult);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(401, loginResult.StatusCode);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("Invalid username or password", loginResult.Value);
+        }
     }
 }
--- backend/BMWapi/Tests/CarExtrasControllerTests.cs
+++ backend/BMWapi/Tests/CarExtrasControllerTests.cs
@@ -1,6 +1,30 @@
-﻿namespace BMWApi.Tests
+﻿using BMWApi.Controllers;
+using BMWApi.Models;
+using BMWApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BMWApi.Tests.Controllers
 {
     public class CarExtrasControllerTests
     {
+        [Fact]
+        public void GetAllCarExtras_ReturnsOk()
+        {
+            // Arrange
+            var mockService = new Mock<ICarExtrasService>();
+            mockService.Setup(service => service.GetAllCarExtras())
+                       .Returns(new List<CarExtras>());
+
+            var controller = new CarExtrasController(mockService.Object);
+
+            // Act
+            var result = controller.GetAllCarExtras();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
     }
 }
