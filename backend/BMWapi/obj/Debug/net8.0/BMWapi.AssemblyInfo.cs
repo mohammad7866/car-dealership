43f25a7a82a7080d29c40abf19424f468eb5b41a
Mohammad A khasiya <mohammad.a.khasiya@uk.pwc.com>
2025-03-26 08:03:56 +0000

    updates csproj

--- backend/BMWapi/obj/Debug/net8.0/BMWapi.AssemblyInfo.cs
+++ backend/BMWapi/obj/Debug/net8.0/BMWapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BMWApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+376459c58041531b8dba5340046bdc73c79327f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a223cb38a98be989a1110abac97f4687c12a6c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BMWApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BMWApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
